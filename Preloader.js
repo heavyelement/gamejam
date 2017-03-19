0e862e2916bc7702f59a78bc52c26d7d6e059da2
Gardiner <gardiner.moody@gmail.com>
2017-03-19 13:40:42 -0400

    Crap-ass prototype

---
+++ Preloader.js
@@ -0,0 +1,43 @@
+
+BasicGame.Preloader = function (game) {
+
+	this.background = null;
+	this.preloadBar = null;
+
+	this.ready = false;
+
+};
+
+BasicGame.Preloader.prototype = {
+
+	preload: function () {
+
+		//	These are the assets we loaded in Boot.js
+		//	A nice sparkly background and a loading progress bar
+
+		this.background = this.add.sprite(0, 0, 'preloaderBackground');
+		this.preloadBar = this.add.sprite(300, 400, 'preloaderBar');
+
+		//	This sets the preloadBar sprite as a loader sprite.
+		//	What that does is automatically crop the sprite from 0 to full-width
+		//	as the files below are loaded in.
+
+		this.load.setPreloadSprite(this.preloadBar);
+
+		//	Here we load the rest of the assets our game needs.
+		//	You can find all of these assets in the Phaser Examples repository
+
+	    this.load.image('volcano', 'assets/cards/card.png');
+	    this.load.image('blank', 'assets/cards/blank.png');
+	    this.load.image('speedcloth', 'assets/board/speedcloth.png');
+		this.load.image('active', 'assets/board/active.png')
+
+	},
+
+	create: function () {
+
+		this.state.start('MainMenu');
+
+	}
+
+};
