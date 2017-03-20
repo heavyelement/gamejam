c25b7f71080de93ec4f93e4e0159a7c0d7b73fa2
egee-irl <brian@egee.io>
2017-03-19 19:24:00 -0700

    comments

--- Game.js
+++ Game.js
@@ -1,6 +1,9 @@
 let game = new Phaser.Game('100%', '100%', Phaser.AUTO, '', { preload: preload, create: create, update: update })
 
 function preload () {
+  // Pre-loading of game assets and some pre-baked logic goes here.
+  // This step is actually mostly optional
+  // It saves the game time before running the create() function
   game.load.image('volcano', 'assets/cards/card.png')
   game.load.image('blank', 'assets/cards/blank.png')
   game.load.image('speedcloth', 'assets/board/speedcloth.png')
@@ -8,6 +11,7 @@ function preload () {
 }
 
 function create () {
+  // This is where the initial state of the game is created
   game.add.sprite(0, 0, 'speedcloth')
   game.add.sprite(800, 500, 'active')
   let volcanoCard = game.add.sprite(0, 0, 'volcano')
@@ -17,4 +21,6 @@ function create () {
 }
 
 function update () {
+  // This is the primary game loop - where pretty much all the game logic will be running
+  // I think this function is called 60 times a second
 }
