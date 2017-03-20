46908a3b56381c0c57282376cc1355a2d5d624fd
egee-irl <brian@egee.io>
2017-03-20 07:37:19 -0700

    Card object, types, and player hand

--- Game.js
+++ Game.js
@@ -1,4 +1,6 @@
 let game = new Phaser.Game('100%', '100%', Phaser.AUTO, '', { preload: preload, create: create, update: update })
+let cardTypes = ['Monster', 'Status', 'Action']
+let hand = []
 
 function preload () {
   // Pre-loading of game assets and some pre-baked logic goes here.
@@ -11,16 +13,22 @@ function preload () {
 }
 
 function create () {
-  // This is where the initial state of the game is created
   game.add.sprite(0, 0, 'speedcloth')
   game.add.sprite(800, 500, 'active')
   let volcanoCard = game.add.sprite(0, 0, 'volcano')
-
   volcanoCard.inputEnabled = 'True'
   volcanoCard.input.enableDrag(true)
+
+  // TODO this 'random' algorithm strongly favors index 0 for some reason - you end up with like 4 or 5 monsters
+  do { hand.push(new Card(cardTypes[Math.floor(Math.random() * hand.length)])) }
+  while (hand.length < 5)
 }
 
 function update () {
-  // This is the primary game loop - where pretty much all the game logic will be running
-  // I think this function is called 60 times a second
+}
+
+class Card {
+  constructor (type) {
+    this.type = type
+  }
 }
