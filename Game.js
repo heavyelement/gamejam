114399b980585fc2786a5a1ad90aa0c23a0b6aa3
egee-irl <brian@egee.io>
2017-03-19 19:19:29 -0700

    Simplified game

--- Game.js
+++ Game.js
@@ -1,52 +1,20 @@
+let game = new Phaser.Game('100%', '100%', Phaser.AUTO, '', { preload: preload, create: create, update: update })
 
-BasicGame.Game = function (game) {
+function preload () {
+  game.load.image('volcano', 'assets/cards/card.png')
+  game.load.image('blank', 'assets/cards/blank.png')
+  game.load.image('speedcloth', 'assets/board/speedcloth.png')
+  game.load.image('active', 'assets/board/active.png')
+}
 
-	//	When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+function create () {
+  game.add.sprite(0, 0, 'speedcloth')
+  game.add.sprite(800, 500, 'active')
+  let volcanoCard = game.add.sprite(0, 0, 'volcano')
 
-    this.game;		//	a reference to the currently running game
-    this.add;		//	used to add sprites, text, groups, etc
-    this.camera;	//	a reference to the game camera
-    this.cache;		//	the game cache
-    this.input;		//	the global input manager (you can access this.input.keyboard, this.input.mouse, as well from it)
-    this.load;		//	for preloading assets
-    this.math;		//	lots of useful common math operations
-    this.sound;		//	the sound manager - add a sound, play one, set-up markers, etc
-    this.stage;		//	the game stage
-    this.time;		//	the clock
-    this.tweens;    //  the tween manager
-    this.state;	    //	the state manager
-    this.world;		//	the game world
-    this.particles;	//	the particle manager
-    this.physics;	//	the physics manager
-    this.rnd;		//	the repeatable random number generator
+  volcanoCard.inputEnabled = 'True'
+  volcanoCard.input.enableDrag(true)
+}
 
-    //	You can use any of these from any function within this State.
-    //	But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
-
-};
-
-BasicGame.Game.prototype = {
-
-	create: function () {
-
-		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-
-	},
-
-	update: function () {
-
-		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-
-	},
-
-	quitGame: function (pointer) {
-
-		//	Here you should destroy anything you no longer need.
-		//	Stop music, delete sprites, purge caches, free resources, all that good stuff.
-
-		//	Then let's go back to the main menu.
-		this.state.start('MainMenu');
-
-	}
-
-};
+function update () {
+}
